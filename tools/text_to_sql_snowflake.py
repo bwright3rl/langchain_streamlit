b55c50b892e949ecfa33c9a4f87e329c8897f2c0
bwright3rl <114755317+bwright3rl@users.noreply.github.com>
2023-10-25 10:06:51 +0100

    adding snowflake details

--- tools/text_to_sql_snowflake.py
+++ tools/text_to_sql_snowflake.py
@@ -6,27 +6,30 @@ from sqlalchemy import create_engine
 from snowflake.sqlalchemy import URL
 from dotenv import load_dotenv
 
+
 def run_query(llm, query):
-    
+
     load_dotenv()
-    
-    account = ''
-    user = ''
-    password = ''
-    warehouse = ''
-    database = ''
-    schema = ''
-    role = ''
-    
-    engine = create_engine(URL(
-        account = account,
-        user = user,
-        password = password,
-        database = database,
-        schema = schema,
-        warehouse = warehouse,
-        role = role
-    ))
+
+    account = "ralphlauren"
+    user = "CIX_REPORTING_USER_PROD"
+    password = "Reporting@CIX-2023"
+    warehouse = "CIX_REPORTING_PROD_WH"
+    database = "CIX_PROD_DB"
+    schema = "FOUNDATION"
+    role = "CIX_DATA_ANALYST_PROD"
+
+    engine = create_engine(
+        URL(
+            account=account,
+            user=user,
+            password=password,
+            database=database,
+            schema=schema,
+            warehouse=warehouse,
+            role=role,
+        )
+    )
 
     _DEFAULT_TEMPLATE = """Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
     
@@ -47,11 +50,18 @@ def run_query(llm, query):
     PROMPT_sql = PromptTemplate(
         input_variables=["input", "table_info", "dialect"], template=_DEFAULT_TEMPLATE
     )
-    
+
     db = SQLDatabase(engine)
 
-    db_chain = SQLDatabaseChain.from_llm(llm, db, prompt=PROMPT_sql, verbose=True, return_intermediate_steps=True, use_query_checker=False)
+    db_chain = SQLDatabaseChain.from_llm(
+        llm,
+        db,
+        prompt=PROMPT_sql,
+        verbose=True,
+        return_intermediate_steps=True,
+        use_query_checker=False,
+    )
 
     response = db_chain({"query": query}, return_only_outputs=True)
 
-    return response['result'], None, response['intermediate_steps'][0]['input']
+    return response["result"], None, response["intermediate_steps"][0]["input"]
